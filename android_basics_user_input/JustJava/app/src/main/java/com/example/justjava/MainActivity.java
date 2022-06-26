1599271f2b88dbd51d96c37d39be4e2cd13b287d
lancherbai <lancher.bai@outlook.com>
2022-06-26 20:58:41 +0800

    Andriod Basics: User Input Object-Oriented Programming: Part 2 Lesson 7-17: EditText for user inputting their name, if/else to add price of whipped cream or chocolate, makeToast to show warnings when ordered cups beyond a certain number.

--- android_basics_user_input/JustJava/app/src/main/java/com/example/justjava/MainActivity.java
+++ android_basics_user_input/JustJava/app/src/main/java/com/example/justjava/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.justjava;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.text.Editable;
 import android.util.Log;
@@ -9,6 +10,7 @@ import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,31 +23,43 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void increment(View view) {
+        Log.v("MainActivity", "Quantity " + quantity);
+
+        if (quantity >= 100) {
+            Toast.makeText(this, "You cannot have more than 100 coffees", Toast.LENGTH_SHORT).show();
+            return;
+        }
         quantity = quantity + 1;
         display(quantity);
-
     }
 
     public void decrement(View view) {
+        Log.v("MainActivity", "Quantity " + quantity);
+
+        if (quantity <= 1) {
+            Toast.makeText(this, "You cannot have less than 1 coffees", Toast.LENGTH_SHORT).show();
+            return;
+        }
         quantity = quantity - 1;
         display(quantity);
-
     }
 
+
     public void submitOrder(View view) {
+        EditText nameField = (EditText) findViewById(R.id.name_field);
+        Editable name = nameField.getText();
+        Log.v("MainActivity", "Name: " + name);
+
         CheckBox whippedCreamCheckBox = (CheckBox) findViewById(R.id.whipped_cream_checkbox);
         boolean hasWhippedCream = whippedCreamCheckBox.isChecked();
+        Log.v("MainActivity", "Has whipped cream: " + hasWhippedCream);
+
         CheckBox chocolateCheckBox = (CheckBox) findViewById(R.id.chocolate_checkbox);
         boolean hasChocolate = chocolateCheckBox.isChecked();
-        EditText getUserName = (EditText) findViewById(R.id.name_field);
-        Editable userName = getUserName.getText();
-
-        Log.v("MainActivity", "Has whipped cream: " + hasWhippedCream);
         Log.v("MainActivity", "Has Chocolate: " + hasChocolate);
-        Log.v("MainActivity", "User Name: " + userName);
 
-        int price = calculatePrice();
-        String priceMessage = createOrderSummary(price, hasWhippedCream, hasChocolate, userName);
+        int price = calculatePrice(hasWhippedCream, hasChocolate);
+        String priceMessage = createOrderSummary(price, hasWhippedCream, hasChocolate, name);
         displayMessage(priceMessage);
     }
 
@@ -60,8 +74,17 @@ public class MainActivity extends AppCompatActivity {
         return priceMessage;
     }
 
-    private int calculatePrice() {
-        return quantity * 5;
+    private int calculatePrice(boolean addWhippedCream, boolean addChocolate) {
+        int basePrice = 5;
+        if (addWhippedCream) {
+            basePrice += 1;
+        }
+
+        if (addChocolate) {
+            basePrice += 2;
+        }
+
+        return quantity * basePrice;
     }
 
     private void display(int number) {
